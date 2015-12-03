1525b89df24985083ff7bee3c3bd17df442843a0
kimxu_work <kimxu.me@gmail.com>
2015-12-03 18:44:06 +0800

    First

--- app/src/main/java/kimxu/newsandfm/frag/FMFragment.java
+++ app/src/main/java/kimxu/newsandfm/frag/FMFragment.java
@@ -1,10 +1,21 @@
 package kimxu.newsandfm.frag;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Message;
 
+import java.io.File;
+
 import kimxu.mvp.databind.DataBinder;
 import kimxu.newsandfm.KBaseFragment;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.functions.Action0;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Fm电台
@@ -47,10 +58,135 @@ public class FMFragment extends KBaseFragment {
     }
 
 
-
     @Override
     protected void bindEvenListener() {
 
+
+        rxAndroid();
+        rxBase();
+        rxAction();
+    }
+
+    private void rxAction() {
+        Action1<String> action1 =new Action1<String>() {
+            @Override
+            public void call(String o) {
+
+            }
+        };
+        Action1 action2 =new Action1() {
+            @Override
+            public void call(Object o) {
+
+            }
+        };
+        Action1 action3 =new Action1() {
+            @Override
+            public void call(Object o) {
+
+            }
+        };
+        Action0 action0 =new Action0() {
+            @Override
+            public void call() {
+
+            }
+        };
+        Observable<String> observable=Observable.just("");
+        observable.subscribe(action1);
+
+    }
+
+    private void rxBase() {
+        //观察者
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String o) {
+
+            }
+        };
+        //等同于上面
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String o) {
+
+            }
+
+            @Override
+            public void onStart() {
+
+                super.onStart();
+            }
+        };
+
+        if (!subscriber.isUnsubscribed()) {
+            subscriber.unsubscribe();
+        }
+
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("Hi");
+                subscriber.onNext("Aloha");
+                subscriber.onCompleted();
+            }
+        });
+        observable.subscribe(observer);
+
+        Observable oJust = Observable.just("s", "s", "s");
+        String[] words = {"Hello", "Hi", "Aloha"};
+        Observable oFrom = Observable.from(words);
+
+}
+
+    private void rxAndroid() {
+        File[] folder=new File[2] ;
+        Observable.from(folder).flatMap(new Func1<File, Observable<File>>() {
+            @Override
+            public Observable<File> call(File file) {
+                return null;
+            }
+        }).filter(new Func1<File, Boolean>() {
+            @Override
+            public Boolean call(File file) {
+                return null;
+            }
+        }).map(new Func1<File, Bitmap>() {
+
+            @Override
+            public Bitmap call(File file) {
+                return null;
+            }
+        })
+          .subscribeOn(Schedulers.io())
+          .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Action1<Bitmap>() {
+            @Override
+            public void call(Bitmap bitmap) {
+
+            }
+        });
     }
 
     @Override
